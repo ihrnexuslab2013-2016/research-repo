5d1c901ebe0b47c0fb31cc5f6ca8527c9ca677e3
Julia Damerow <julia.damerow@googlemail.com>
2015-07-24 16:07:24 -0700

    RERE-10: export first have of properties to MODS XML

--- app/models/export_mods.rb
+++ app/models/export_mods.rb
@@ -16,7 +16,97 @@ module ExportMODS
             xml.title title.label
           }
         end
-      # end modsCollection
+        
+        # add abstract
+        abstract.each do |abstract|
+          xml.abstract abstract
+        end
+        
+        # add access conditions
+        access_condition.each do |cond|
+          xml.accessCondition cond
+        end
+        
+        # add genres
+        genres.each do |genre|
+          xml.genre genre.label
+        end
+        
+        # add language
+        language_of_resources.each do |lang|
+          xml.language {
+            xml.languageTerm(:type => "code", :authority => "iso639-2b"){ xml.text(lang) }
+          }
+        end
+        
+        # add locations
+        location_of_resources.each do |loc|
+          xml.location {
+            if !loc.location_physical.nil? and !loc.location_physical.blank?
+              xml.physicalLocation loc.location_physical
+            end
+            if !loc.location_shelf_locator.nil? and !loc.location_shelf_locator.blank?
+              xml.shelfLocator loc.location_shelf_locator
+            end
+            if !loc.url.nil? and !loc.url.blank?
+              xml.url loc.url
+            end
+          }
+        end
+        
+        # add names
+        # todo: add other properties
+        names.each do |name|
+          xml.name {
+            xml.namePart name.label
+          }
+        end
+        
+        # add notes
+        notes.each do |note|
+          xml.note note
+        end
+        
+        # add statement of responsibility
+        statement_of_responsibility.each do |statement|
+          xml.note(:type => "statement of responsibility") { xml.text(statement) }          
+        end
+        
+        # add typed notes
+        note_groups.each do |note|
+          xml.note(:type => note.note_group_type) { xml.text(note.note_group_note) }
+        end
+        
+        # add edition
+        edition.each do |edition|
+          xml.originInfo {
+            xml.edition edition
+          }
+        end
+        
+        #add frequency
+        frequency.each do |freq|
+          xml.originInfo {
+            xml.frequency freq
+          }
+        end
+        
+        date_issued.each do |date|
+          xml.originInfo {
+            xml.dateIssued date
+          }
+        end
+        
+        parts.each do |part|
+          xml.part(:order => part.part_order) {
+            xml.detail(:level => part.part_level) {
+              xml.number part.part_number
+              xml.caption part.part_caption
+            }
+          }
+        end
+        
+      # end mods
       }
     end
     
--- spec/models/export_spec.rb
+++ spec/models/export_spec.rb
@@ -13,6 +13,19 @@ describe GenericFile do
     before {
       file.title_principals_attributes = [{label: "First title"}]
       file.title_uniforms_attributes = [{label: "First uniform title"}]
+      file.abstract = ["Test abstract with a few more words."]
+      file.access_condition = ["Personal, noncommercial use of this item is permitted in the United States of America. Please see http://digital.library.upenn.edu/women/ for other rights and restrictions that may apply to this resource."]
+      file.genres_attributes = [{label: "Web site"}]
+      file.language_of_resources = ["eng"]
+      file.location_of_resources_attributes = [{location_physical: "Indiana University, Bloomington. University Archives P07803", location_shelf_locator: "HB123.89" }, { url: "http://purl.dlib.indiana.edu/iudl/archives/cushman/P07803" }]
+      file.names_attributes =[{label: "Raschke, Gregory K."}]
+      file.notes = ["Dominus vo-bisque'em et come spear a tu-tu, oh."]
+      file.statement_of_responsibility = ["created and maintained by the Asian Division, Area Studies Directorate"]
+      file.note_groups_attributes = [{note_group_note: "Received: 6/17/82; ref print; gift, ATM 138; AFI/Woods (John) Collection.", note_group_type: "acquisition"}]
+      file.edition = ["Limited ed"]
+      file.frequency = ["Annual"]
+      file.date_issued = ["1889"]
+      file.parts_attributes = [{part_order: "2", part_level: "no. 3", part_caption: "Caption", part_number: "Part 1"}]
     }
     subject(:xml){ file.export_as_mods_xml }
     it {
